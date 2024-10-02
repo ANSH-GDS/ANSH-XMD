b305d7eda2948b482dd23b837df536e873d9895d
Sufiyan don <sufiandar372@gmail.com>
2024-10-02 23:49:40 +0530

    Update main-runtime.js

--- plugins/main-runtime.js
+++ plugins/main-runtime.js
@@ -10,7 +10,7 @@ m.react('⏳')
       }) * 1000
     }
     let muptime = clockString(_muptime)
-    let str = `🟢ᴘʀɪɴᴄᴇ ᴍᴅ ᴜᴘᴛɪᴍᴇ\n\n🎗️ʙᴏᴛ ᴀᴄᴛɪᴠᴇ ᴅᴜʀᴀᴛɪᴏɴ\n *${muptime}⏰*`
+    let str = `🟢ʙᴀɴɴᴏ ᴍᴅ ᴜᴘᴛɪᴍᴇ\n\n🎗️ʙᴏᴛ ᴀᴄᴛɪᴠᴇ ᴅᴜʀᴀᴛɪᴏɴ\n *${muptime}⏰*`
     conn.sendMessage(m.chat, {
       text: str,
       contextInfo: {
@@ -24,8 +24,8 @@ m.react('⏳')
             },
       forwardingScore: 999,
       externalAdReply: {
-      title: "🎗️ᴘʀɪɴᴄᴇ ᴍᴅ🎗️",
-      body: "ʀᴜɴɪɴɢ sɪɴᴄᴇ",
+      title: "🎗️ʙᴀɴɴᴏ ᴍᴅ🎗️",
+      body: "ʀᴜɴɪɴɢ ʙᴀɴɴᴏ",
       thumbnailUrl: pp,
       sourceUrl: '',
       mediaType: 1,

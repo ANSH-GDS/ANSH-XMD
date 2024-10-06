ff69a71acc198bdd73cae834b80ddf860ef36fa2
ANSH-GDS <anshansh84796@gmail.com>
2024-10-06 17:05:45 +0530

    𝗧𝗛𝗘-𝗔𝗡𝗦𝗛-𝗕𝗢𝗧

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -3,7 +3,7 @@ import { join } from 'path';
 import axios from 'axios'; 
 
 let handler = async function (m, { conn, __dirname }) {
-  const githubRepoURL = 'https://github.com/ANSH-GDS/ANSH-MD';
+  const githubRepoURL = 'https://github.com/ANSH-GDS/ANSH-XMD';
 
   try {
   
@@ -16,7 +16,7 @@ let handler = async function (m, { conn, __dirname }) {
 
       // Format the repository information with emojis
       const formattedInfo = `
-╭⊱✫💌𝘼𝙉𝙎𝙃-𝙈𝘿💌✫⊱╮
+╭⊱✫💌𝘼𝙉𝙎𝙃-𝙓𝙈𝘿💌✫⊱╮
 │✫ - *📂Repository Name:* ${repoData.name}
 │✫ - *📃Description:* ${repoData.description}
 │✫ - *🛡️Owner:* ${repoData.owner.login}
